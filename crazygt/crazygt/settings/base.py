521e6d941a56865c3b7410b51acf8161631217a1
itw_hongww <itw_hongww@tkmail.com>
2018-06-22 09:56:37 +0800

    添加访问ip

--- crazygt/crazygt/settings/base.py
+++ crazygt/crazygt/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '1%(u4z4&+ev^8h+3+qk!nnctm&s%6v-ru3pjb88u%90bj0llv$'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.weiax.xin']
 
 # Application definition
 
